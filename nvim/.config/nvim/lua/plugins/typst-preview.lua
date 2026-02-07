5b5321cc5649f31bb5acf9c34d18249a32427f03
Alvinceleste2 <github.hvgyj15x@alvinceleste.eu>
2026-02-07 14:24:13 +0100

    feat(nvim): nixos typst-preview plugin config

--- nvim/.config/nvim/lua/plugins/typst-preview.lua
+++ nvim/.config/nvim/lua/plugins/typst-preview.lua
@@ -2,5 +2,11 @@ return {
   "chomosuke/typst-preview.nvim",
   lazy = false, -- or ft = 'typst'
   version = "1.*",
-  opts = {}, -- lazy.nvim will implicitly calls `setup {}`
+  opts = {
+    -- The following only for NixOS
+    dependencies_bin = {
+      tinymist = "/run/current-system/sw/bin/tinymist",
+      websocat = "/run/current-system/sw/bin/websocat",
+    },
+  }, -- lazy.nvim will implicitly calls `setup {}`
 }
